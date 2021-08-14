0702a68f1861067d7690a8e008cbd2b60ea50b7c
RizqiSyahrendra <rizqialaudinsyahrendra@gmail.com>
2021-08-14 20:42:21 +0700

    scroll chat screen to bottom

--- src/components/Chatscreen.js
+++ src/components/Chatscreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useEffect } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import Chatitem from './Chatitem'
 import { StoreContext } from '../store'
@@ -6,18 +6,23 @@ import { StoreContext } from '../store'
 const Chatscreen = () => {
     const { stateActiveChat } = useContext(StoreContext);
     const { user } = stateActiveChat;
+    const chatScreenRef = useRef(null);
+    
+    useEffect(() => {
+        chatScreenRef.current?.scrollIntoView();
+    }, [stateActiveChat]);
 
     return (
         <div className="py-2 chat-screen">
-        {
-            stateActiveChat.chatList.map((item, idxChat) => (
-                <Row key={idxChat}>
-                    <Col sm={12} md={12} lg={12} className="px-3 py-1 my-1">
-                        <Chatitem data={item} friend={user} />
-                    </Col>
-                </Row>
-            ))
-        }
+            {
+                stateActiveChat.chatList.map((item, idxChat) => (
+                    <Row key={idxChat} ref={chatScreenRef}>
+                        <Col sm={12} md={12} lg={12} className="px-3 py-1 my-1">
+                            <Chatitem data={item} friend={user} />
+                        </Col>
+                    </Row>
+                ))
+            }
         </div>
     )
 }
